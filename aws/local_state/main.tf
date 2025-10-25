6463226e0630d6363d7cf39f10a4cce8a060772a
niravrafaliya <rafalia_nirav@yahoo.com>
2025-10-25 17:40:36 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,8 +14,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (x86_64)
-  instance_type = "t3.micro"
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_Demo"
