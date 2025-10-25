c41bee35aadc83d1dd2b492842352493c04db724
niravrafaliya <rafalia_nirav@yahoo.com>
2025-10-25 17:24:01 +0530

    Update AMI ID for app_server resource

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,11 +14,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
