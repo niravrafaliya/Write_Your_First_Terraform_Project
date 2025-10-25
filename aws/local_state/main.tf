afa013ab77d459ebdbf1692943ede98aaf6dd860
niravrafaliya <rafalia_nirav@yahoo.com>
2025-10-25 18:25:44 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-03aa99ddf5498ceb9"
   instance_type = "t3.micro"
 
   tags = {
